584d40ca7e431892c8fd55225140f3875b97f4d4
rawanselima <115345742+rawanselima@users.noreply.github.com>
2025-09-04 05:32:11 +0300

    update path home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import HeroSection from "../components/home/HeroSection";
 import Category from "../components/home/Category";
 import Banner from "../components/home/Banner";
 import TopSales from "../components/home/TopSales";
-import Feedback from "../components/home/FeedBack";
+import Feedback from "../components/home/Feedback";
 import Contact from "../components/home/Contact";
 
 const Home = () => {
